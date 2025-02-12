faf33fe1699f75abaa5484e8feaa43064647ae26
JeppeSiegf <10438303@ucn.dk>
2025-02-12 17:12:33 +0100

    ci test

--- filldb.py
+++ filldb.py
@@ -21,7 +21,7 @@ async def update_all_films():
         await FilmService.update_film(film.page_ref)
         print(count)
         count += 1
-        logic *= 3
+        logic *= 9
 
 
 async def add_users():
